142b4153f19bb8e7dbd57fd697117b95fae1db81
adminverin <test@averin.co.id>
2023-07-31 14:48:39 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,20 @@ import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
-  runApp(
-    GetMaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
-    ),
-  );
+    );
+  }
 }
